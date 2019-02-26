26302edc3dbbde03f2fcc4e73b1c4a730e4556b6
Alfredo Campelo <egofrido@gmail.com>
2019-02-26 14:11:28 +0100

    saludo

--- mod1_types_sentences.js
+++ mod1_types_sentences.js
@@ -1,1 +1,13 @@
 // Type your code here
+//Línea en blanco
+//Línea con saludo
+let saludo;
+let hora = new Date().getHours();
+if(hora >6 && hora < 13){
+	saludo = '\n Buenos días';
+}else if(hora > 13 && hora < 22){
+	saludo = '\n Buenas tardes';
+}else{
+	saludo = '\n Buenas noches'
+}
+console.log(`${saludo}, son las ${hora} horas`);
