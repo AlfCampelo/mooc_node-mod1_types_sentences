8088da982c653693106c4f5445e7758bc28aa223
Alfredo Campelo <egofrido@gmail.com>
2019-02-26 15:09:17 +0100

    tabla dec-hex-oct-bin

--- mod1_types_sentences.js
+++ mod1_types_sentences.js
@@ -1,5 +1,3 @@
-// Type your code here
-//Línea en blanco
 //Línea con saludo
 let saludo;
 let hora = new Date().getHours();
@@ -11,6 +9,20 @@ if(hora >6 && hora < 13){
 	saludo = '\n Buenas noches'
 }
 console.log(`${saludo}, son las ${hora} horas`);
+
 //Número PI con seis decimales
 let numPi = Math.PI.toFixed(6);
 console.log(`\n Número PI con seis decimales: ${numPi}`);
+
+//Tabla con números de 0 a 22 en decimal, hexadecimal, octal y binario
+let numero = 22;
+function tablaNumerica(n){
+	let hex = (n).toString(16);
+	let oct = (n).toString(8);
+	let bin = (n).toString(2);
+	console.log(`\n ${n} dec = ${hex} hex = ${oct} oct = ${bin} bin`);
+}
+
+for(let i = 0; i <= numero; i++){
+	tablaNumerica(i);
+}
